a54f2a5a61ece97aba9b51d3f9cad2271d741d61
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-11-07 16:24:04 -0500

    heroku bug32

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 const db = config.get('mongoURI');
 
 mongoose
-    .connect(db, {
+    .connect(process.env.db, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
