8d9075e9d98d71adb94aea5064b6abc8e8353d05
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-11-07 18:11:44 -0500

    heroku bug3442

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/emailEvents', require("./routes/api/emailEvents"))
 app.use('/api/events', require("./routes/api/events"))
 
 
-if (process.env.Node_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
