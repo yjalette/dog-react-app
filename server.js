654203dce245b017bbaa4b0030bc48542b44a374
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-11-07 16:42:46 -0500

    heroku bug32

--- server.js
+++ server.js
@@ -5,25 +5,18 @@ const config = require('config');
 const app = express();
 var cors = require('cors');
 
-app.use(cors());
-
-if(process.env.Node_ENV === 'production'){
-    app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-}
+// app.use(cors());
 
 app.use(express.json())
 
-const db = process.env.config.get('mongoURI');
+const db = config.get('mongoURI');
 
 mongoose
     .connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
-        
+
     })
     .then(() => console.log("mongo is connecting"))
     .catch((err) => console.log("this is the errorrrrr:", err))
@@ -34,7 +27,16 @@ app.use('/api/auth', require("./routes/api/auth"))
 app.use('/api/emailEvents', require("./routes/api/emailEvents"))
 app.use('/api/events', require("./routes/api/events"))
 
-const port = process.env.PORT || 5000; 
+
+if (process.env.Node_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
+
+const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => console.log(`server started on ${port}`))
