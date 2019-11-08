abaf70ed70b6eb981f0a364e0621dfc852870cda
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-11-07 19:48:09 -0500

    heroku html email

--- config/domain.js
+++ config/domain.js
@@ -2,5 +2,3 @@ module.exports = {
     host: process.env.NODE_ENV ==='production' ? process.env.HOST : process.env.LOCAL_HOST
 }
 
-// "start": "SET NODE_ENV=production && node server.js",
-//     "server": "SET NODE_ENV=development && nodemon server.js --ignore client",
